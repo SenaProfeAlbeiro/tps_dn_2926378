c96ff179f5493f9ebaf3949358ab3a4255889b42
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-09-07 16:14:41 -0600

    6. Implementación de las vistas

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
     require_once "models/DataBase.php";
-    require_once "controllers/Users.php";
-    $controller = new Users;
-    $controller->create_user();
+    if (!isset($_REQUEST['c'])) {
+        require_once "controllers/Landing.php";
+        $controller = new Landing;
+        $controller->main();
+    } else {
+        $controller = $_REQUEST['c'];
+        require_once "controllers/" . $controller . ".php";
+        $controller = new $controller;
+        $action = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'main';
+        call_user_func(array($controller, $action));
+    }
 ?>
