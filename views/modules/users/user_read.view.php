85bf09d6954bcc0c297390a6d2486be7dea9bf0f
“SenaProfeAlbeiro” <earamos42@misena.edu.co>
2024-11-05 21:22:04 -0500

    2. Archivos de Arranque

--- views/modules/users/user_read.view.php
+++ views/modules/users/user_read.view.php
@@ -1,56 +1,56 @@
-    <h1>Consultar Usuarios</h1>
-    <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-            <th scope="col" class="text-center">Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td class="text-center">
-              <a href="user_update.html" class="editar">
-                <i class="fa-solid fa-pen-to-square"></i>
-              </a>
-              <a href="#" class="eliminar">
-                <i class="fa-solid fa-trash-can"></i>
-              </a>
-          </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td class="text-center">
-              <a href="user_update.html" class="editar">
-                  <i class="fa-solid fa-pen-to-square"></i>
-                </a>
-                <a href="#" class="eliminar">
-                  <i class="fa-solid fa-trash-can"></i>
-                </a>
-            </td>
-          </tr>          
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-            <td class="text-center">
-              <a href="user_update.html" class="editar">
-                <i class="fa-solid fa-pen-to-square"></i>
-              </a>
-              <a href="#" class="eliminar">
-                <i class="fa-solid fa-trash-can"></i>
-              </a>
-          </td>
-          </tr>
-        </tbody>
-      </table>
+              <h1>Consultar Usuarios</h1>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    <th scope="col" class="text-center">Acciones</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td class="text-center">
+                      <a href="user_update.html" class="editar">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                      </a>
+                      <a href="#" class="eliminar">
+                        <i class="fa-solid fa-trash-can"></i>
+                      </a>
+                  </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td class="text-center">
+                      <a href="user_update.html" class="editar">
+                          <i class="fa-solid fa-pen-to-square"></i>
+                        </a>
+                        <a href="#" class="eliminar">
+                          <i class="fa-solid fa-trash-can"></i>
+                        </a>
+                    </td>
+                  </tr>          
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td class="text-center">
+                      <a href="user_update.html" class="editar">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                      </a>
+                      <a href="#" class="eliminar">
+                        <i class="fa-solid fa-trash-can"></i>
+                      </a>
+                  </td>
+                  </tr>
+                </tbody>
+              </table>
